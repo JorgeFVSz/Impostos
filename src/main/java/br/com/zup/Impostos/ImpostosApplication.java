f306354f1872bacff2de1f5b7571b72edbbbe788
JorgeFVSz <jorge.silvestre@zup.com.br>
2025-03-04 12:11:44 -0300

    chore: starting project from spring initializr with dependencies: spring web, jpa, postgresql driver, validation

---
+++ src/main/java/br/com/zup/Impostos/ImpostosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.Impostos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpostosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpostosApplication.class, args);
+	}
+
+}
