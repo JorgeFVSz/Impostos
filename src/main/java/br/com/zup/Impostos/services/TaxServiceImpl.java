c05edf9f87847a04fffb7ec18e79238f0c7fc121
JorgeFVSz <jorge.silvestre@zup.com.br>
2025-03-05 17:29:52 -0300

    feat: add getAllTaxes method to TaxServiceImpl

--- src/main/java/br/com/zup/Impostos/services/TaxServiceImpl.java
+++ src/main/java/br/com/zup/Impostos/services/TaxServiceImpl.java
@@ -1,11 +1,22 @@
 package br.com.zup.Impostos.services;
 
+import br.com.zup.Impostos.dtos.TaxDTO;
+import br.com.zup.Impostos.models.Tax;
 import br.com.zup.Impostos.repositories.TaxRepository;
+import br.com.zup.Impostos.utils.TaxMapperUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaxServiceImpl implements TaxService{
     @Autowired
     private TaxRepository taxRepository;
+
+    @Override
+    public List<TaxDTO> getAllTaxes() {
+        List<Tax> taxesEntity = taxRepository.findAll();
+        return TaxMapperUtil.toDTOList(taxesEntity);
+    }
 }
