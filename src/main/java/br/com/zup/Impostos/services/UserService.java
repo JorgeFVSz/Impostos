425511c60494e4b04f99385c87b522cfd2e8ac9f
JorgeFVSz <jorge.silvestre@zup.com.br>
2025-03-05 10:34:14 -0300

    feat: add UserService with repository attribute

--- src/main/java/br/com/zup/Impostos/services/UserService.java
+++ src/main/java/br/com/zup/Impostos/services/UserService.java
@@ -1,4 +1,11 @@
 package br.com.zup.Impostos.services;
 
+import br.com.zup.Impostos.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
 }
