b13f64d235ee9fd9e9f7e1b39a024d5cf43434e2
JorgeFVSz <jorge.silvestre@zup.com.br>
2025-03-05 14:08:07 -0300

    feat: add TaxRepository interface

---
+++ src/main/java/br/com/zup/Impostos/repositories/TaxRepository.java
@@ -0,0 +1,9 @@
+package br.com.zup.Impostos.repositories;
+
+import br.com.zup.Impostos.models.Tax;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaxRepository extends JpaRepository<Tax, String> {
+}
