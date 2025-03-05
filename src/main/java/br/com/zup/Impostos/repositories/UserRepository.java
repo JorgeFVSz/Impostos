fd516c84b6f01653997c93855968d2bf91866f31
JorgeFVSz <jorge.silvestre@zup.com.br>
2025-03-05 10:21:30 -0300

    feat: add file UserRepository.java

---
+++ src/main/java/br/com/zup/Impostos/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.Impostos.repositories;
+
+import br.com.zup.Impostos.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
