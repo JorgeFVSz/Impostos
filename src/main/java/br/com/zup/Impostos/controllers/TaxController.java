159e90d647cc4f65c78fd1280de87fe20ddf2fb8
JorgeFVSz <jorge.silvestre@zup.com.br>
2025-03-05 17:17:34 -0300

    feat: add getAllTaxes method to controller

--- src/main/java/br/com/zup/Impostos/controllers/TaxController.java
+++ src/main/java/br/com/zup/Impostos/controllers/TaxController.java
@@ -1,11 +1,22 @@
 package br.com.zup.Impostos.controllers;
 
+import br.com.zup.Impostos.dtos.TaxDTO;
 import br.com.zup.Impostos.services.TaxService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TaxController {
     @Autowired
     private TaxService taxService;
+
+    @GetMapping("/tipos")
+    public ResponseEntity<List<TaxDTO>> getAllTaxes() {
+        List<TaxDTO> taxes = taxService.getAllTaxes();
+        return ResponseEntity.ok(taxes);
+    }
 }
