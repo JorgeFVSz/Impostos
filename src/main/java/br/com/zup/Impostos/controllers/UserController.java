6adc78ca33668fad3754177d9c0537d7748f62dc
JorgeFVSz <jorge.silvestre@zup.com.br>
2025-03-05 11:27:15 -0300

    feat: add UserController with UserService attribute

--- src/main/java/br/com/zup/Impostos/controllers/UserController.java
+++ src/main/java/br/com/zup/Impostos/controllers/UserController.java
@@ -1,4 +1,13 @@
 package br.com.zup.Impostos.controllers;
 
+import br.com.zup.Impostos.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
 }
