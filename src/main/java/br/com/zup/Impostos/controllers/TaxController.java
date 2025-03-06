3a1e7a8c9422b0e4811e8119ff59f1399ca8c4bf
JorgeFVSz <jorge.silvestre@zup.com.br>
2025-03-06 13:20:12 -0300

    feat: add createTax method to controller

--- src/main/java/br/com/zup/Impostos/controllers/TaxController.java
+++ src/main/java/br/com/zup/Impostos/controllers/TaxController.java
@@ -3,8 +3,11 @@ package br.com.zup.Impostos.controllers;
 import br.com.zup.Impostos.dtos.TaxDTO;
 import br.com.zup.Impostos.services.TaxService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +21,11 @@ public class TaxController {
     public ResponseEntity<List<TaxDTO>> getAllTaxes() {
         List<TaxDTO> taxes = taxService.getAllTaxes();
         return ResponseEntity.ok(taxes);
+    }
+
+    @PostMapping("/tipos")
+    public ResponseEntity<TaxDTO> createTax(@RequestBody TaxDTO taxDTO) {
+        TaxDTO taxResonseDTO = taxService.createTax(taxDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(taxResonseDTO);
     }
 }
