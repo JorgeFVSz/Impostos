37ff89918517d5ca7e6528418c14a3406b613523
JorgeFVSz <jorge.silvestre@zup.com.br>
2025-03-06 13:23:11 -0300

    feat: add taxType attribute getters and setters and constructor in DTO

--- src/main/java/br/com/zup/Impostos/dtos/TaxDTO.java
+++ src/main/java/br/com/zup/Impostos/dtos/TaxDTO.java
@@ -1,10 +1,23 @@
 package br.com.zup.Impostos.dtos;
 
+import br.com.zup.Impostos.enums.TaxType;
+
 public class TaxDTO {
     private String id;
     private String nome;
     private String descricao;
     private Double aliquota;
+    private TaxType taxType;
+
+    public TaxDTO() {
+    }
+
+    public TaxDTO(String id, String nome, String descricao, Double aliquota) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.aliquota = aliquota;
+    }
 
     public String getId() {
         return id;
@@ -36,5 +49,13 @@ public class TaxDTO {
 
     public void setAliquota(Double aliquota) {
         this.aliquota = aliquota;
+    }
+
+    public TaxType getTaxType() {
+        return taxType;
+    }
+
+    public void setTaxType(TaxType taxType) {
+        this.taxType = taxType;
     }
 }
