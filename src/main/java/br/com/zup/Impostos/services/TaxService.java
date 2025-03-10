2ab1e0998c28cd258ee27e2c77da287cd6f86d26
JorgeFVSz <jorge.silvestre@zup.com.br>
2025-03-10 15:23:34 -0300

    feat: add the signature of the calculateTax method in the TaxService interface

--- src/main/java/br/com/zup/Impostos/services/TaxService.java
+++ src/main/java/br/com/zup/Impostos/services/TaxService.java
@@ -1,5 +1,7 @@
 package br.com.zup.Impostos.services;
 
+import br.com.zup.Impostos.dtos.TaxCalculationRequestDTO;
+import br.com.zup.Impostos.dtos.TaxCalculationResponseDTO;
 import br.com.zup.Impostos.dtos.TaxDTO;
 
 import java.util.List;
@@ -7,4 +9,5 @@ import java.util.List;
 public interface TaxService {
     List<TaxDTO> getAllTaxes();
     TaxDTO createTax(TaxDTO taxDTO);
+    TaxCalculationResponseDTO calculateTax(TaxCalculationRequestDTO taxCalculationRequestDTO);
 }
